3a68dc030277c6f5cd32555b2ab15e298237ec3f
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-07-26 01:43:01 +0200

    Added repository remove command

--- internal/data/configs.go
+++ internal/data/configs.go
@@ -72,6 +72,48 @@ func AddRepository(name string, remote string) error {
 	return nil
 }
 
+func RemoveRepository(name string) error {
+	repositories, _ := GetRepositoryData()
+	if repositories == nil {
+		return errors.New("No repository available")
+	}
+
+	deleted := false
+	for i, repository := range repositories {
+		if repository.Name == name {
+			repositories = append(repositories[:i], repositories[i+1:]...)
+			deleted = true
+		}
+	}
+
+	if !deleted {
+		return errors.New("Could not find repository with that name.")
+	}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println(err.Error())
+
+		return errors.New("Could not find a home directory.")
+	}
+
+	jsonData, err := json.Marshal(repositories)
+	if err != nil {
+		fmt.Println(err.Error())
+
+		return errors.New("Could not parse data to json")
+	}
+
+	if err := os.WriteFile(homeDir+"/.local/share/tmpl/repositories.json", jsonData, 0750); err != nil {
+		fmt.Println(err.Error())
+
+		return errors.New("Could not write to file")
+	}
+
+	return nil
+
+}
+
 func createMissing(path string) error {
 	if err := os.MkdirAll(path, 0750); err != nil {
 		fmt.Println(err.Error())
