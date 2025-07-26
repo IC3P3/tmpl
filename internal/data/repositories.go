41cd5b6dc1a308c000af6ceb062c4d882afdc9dd
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-07-27 01:20:28 +0200

    Added repository and template data get functions

--- internal/data/repositories.go
+++ internal/data/repositories.go
@@ -1,10 +1,35 @@
 package data
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
 )
+
+type TemplateConfig struct {
+	Displayname string
+	Name        string
+	Description string
+	Prehook     []string
+	Posthook    []string
+}
+
+type AddonConfig struct{}
+
+type HookConfig struct {
+	Displayname string
+	Name        string
+	Description string
+}
+
+type RepositoryConfig struct {
+	Displayname string
+	Description string
+	Templates   []TemplateConfig
+	Addons      []AddonConfig
+	Hooks       []HookConfig
+}
 
 func DeleteRepository(name string) error {
 	homeDir, err := os.UserHomeDir()
@@ -23,4 +48,54 @@ func DeleteRepository(name string) error {
 	fmt.Printf("Successfully deleted %s from the list.\n", name)
 
 	return nil
+}
+
+func GetRepositoryDescription(name string) (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println(err.Error())
+
+		return "", errors.New("Could not find a home directory.")
+	}
+
+	file, err := os.ReadFile(homeDir + "/.local/share/tmpl/" + name + "/tmpl.json")
+	if err != nil {
+		fmt.Println(err.Error())
+
+		return "", errors.New("No file or permission to read the file.")
+	}
+
+	repositoryContent := RepositoryConfig{}
+	if err := json.Unmarshal(file, &repositoryContent); err != nil {
+		fmt.Println(err.Error())
+
+		return "", errors.New("Could not parse repository.json")
+	}
+
+	return repositoryContent.Description, nil
+}
+
+func GetTemplateData(repoName string) ([]TemplateConfig, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println(err.Error())
+
+		return []TemplateConfig{}, errors.New("Could not find a home directory.")
+	}
+
+	file, err := os.ReadFile(homeDir + "/.local/share/tmpl/" + repoName + "/tmpl.json")
+	if err != nil {
+		fmt.Println(err.Error())
+
+		return []TemplateConfig{}, errors.New("No file or permission to read the file.")
+	}
+
+	repositoryContent := RepositoryConfig{}
+	if err := json.Unmarshal(file, &repositoryContent); err != nil {
+		fmt.Println(err.Error())
+
+		return []TemplateConfig{}, errors.New("Could not parse repository.json")
+	}
+
+	return repositoryContent.Templates, nil
 }
