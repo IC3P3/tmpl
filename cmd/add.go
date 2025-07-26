8086c56d4893f316e691013282d11d9f01de153d
IC3P3 <50029892+IC3P3@users.noreply.github.com>
2025-07-26 12:46:57 +0200

    Implemented add command

--- cmd/add.go
+++ cmd/add.go
@@ -2,17 +2,29 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/IC3P3/tmpl/internal/data"
+	"github.com/IC3P3/tmpl/internal/git"
 	"github.com/spf13/cobra"
 )
 
 var addCmd = &cobra.Command{
-	Use:   "add",
+	Use:   "add <Name> <Repository Remote>",
+	Args:  cobra.ExactArgs(2),
 	Short: "Add a new repository containing templates",
 	Long: `The add command adds a new repository to the list of
 available templates.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Not implemented yet!")
+		if err := data.AddRepository(args[0], args[1]); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := git.GetTemplateRepository(args[0], args[1]); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("The repository '%s' was successfully cloned and added.\n", args[0])
 	},
 }
 
